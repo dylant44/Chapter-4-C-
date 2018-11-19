60cbe7bbaf34f51246c8dd259f0c51caf22e0990
dylant44 <dt303271@students.davenport.k12.ia.us>
2018-11-19 12:18:02 -0600

    if else quiz

---
+++ Age Unsult Generator/Age Unsult Generator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Age_Unsult_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int yes;
+            string yesString;
+            string ageString;
+            Write("Welcome to the famous Age Insult Generator!!!");
+            Write("Please enter your age");
+            ageString = ReadLine();
+            age = Convert.ToInt32(ageString);
+
+            if (age < 19)
+            {
+                Write("You are a minor");
+            }
+            else if (age >= 18 && age < 25)
+            {
+                Write("You should be in college or done with college by now. dont be a deadbeat");
+            }
+            else if (age >= 25 && age < 32)
+            {
+                Write("Do you have a job? 1 for yes 2 for no");
+                yesString = ReadLine();
+                yes = Convert.ToInt32(yesString);
+                if (yes == 1)
+                {
+                    Write("Good for you");
+                }
+                else
+                {
+                    Write("Get a job hippie");
+                }
+
+            if(age >= 32 && age < 42)
+                {
+                    Write("You are getting old");
+                }
+            else if(age >= 42 && age < 52)
+                {
+                    Write("You are getting really old");
+                }
+            else if(age >= 52 && age < 62)
+                {
+                    Write("Dude you're ancient");
+                }
+            else if(age >= 62)
+                {
+                    Write("get off your grandkids computer");
+                }
+             
+                
+            }
+
+        }
+    }
+}
---
+++ IfElseQuiz/IfElseQuiz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace IfElseQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userChoice;
+            string userChoiceString;
+            WriteLine("Please enter a number: 1, 2, or 3");
+            userChoiceString = ReadLine();
+            userChoice = Convert.ToInt32(userChoiceString);
+
+            if(userChoice == 1 || userChoice == 3)
+            {
+                WriteLine("You Lose!");
+            }
+            else if(userChoice == 2)
+            {
+                WriteLine("You Win!");
+            }
+            else
+            {
+                WriteLine("ERROR");
+            }
+
+        }
+    }
+}
