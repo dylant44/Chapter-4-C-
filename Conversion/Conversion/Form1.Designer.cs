d1d72a4029d5cf16f019e27e04de722acc29cf05
dylant44 <dt303271@students.davenport.k12.ia.us>
2018-11-20 13:12:09 -0600

    FahrenHeit to Celcius

--- Conversion/Conversion/Form1.Designer.cs
+++ Conversion/Conversion/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.lblTitle = new System.Windows.Forms.Label();
             this.txtTemp8 = new System.Windows.Forms.TextBox();
-            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnFahrenheitConversion = new System.Windows.Forms.Button();
             this.lblCelsius8Title = new System.Windows.Forms.Label();
             this.lblCelsius12Title = new System.Windows.Forms.Label();
             this.lblCelsius5Title = new System.Windows.Forms.Label();
@@ -42,6 +42,23 @@
             this.lblResult8 = new System.Windows.Forms.Label();
             this.lblResult12 = new System.Windows.Forms.Label();
             this.lblResult5 = new System.Windows.Forms.Label();
+            this.lblAverageFahrenheit = new System.Windows.Forms.Label();
+            this.lblResultAverageFah = new System.Windows.Forms.Label();
+            this.lblTitleCel8 = new System.Windows.Forms.Label();
+            this.lblTitleCel12 = new System.Windows.Forms.Label();
+            this.lblTitleCel5 = new System.Windows.Forms.Label();
+            this.lblAverageCel = new System.Windows.Forms.Label();
+            this.lblResultCel8 = new System.Windows.Forms.Label();
+            this.lblResultCel12 = new System.Windows.Forms.Label();
+            this.lblResultCel5 = new System.Windows.Forms.Label();
+            this.lblResultAverageCel = new System.Windows.Forms.Label();
+            this.btnCelsiusConversion = new System.Windows.Forms.Button();
+            this.lblMessageF1 = new System.Windows.Forms.Label();
+            this.lblMessageF2 = new System.Windows.Forms.Label();
+            this.lblMessageF3 = new System.Windows.Forms.Label();
+            this.lblMessageC1 = new System.Windows.Forms.Label();
+            this.lblMessageC2 = new System.Windows.Forms.Label();
+            this.lblMessageC3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -60,42 +77,42 @@
             this.txtTemp8.Size = new System.Drawing.Size(100, 20);
             this.txtTemp8.TabIndex = 1;
             // 
-            // btnConvert
+            // btnFahrenheitConversion
             // 
-            this.btnConvert.Location = new System.Drawing.Point(112, 214);
-            this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(121, 23);
-            this.btnConvert.TabIndex = 2;
-            this.btnConvert.Text = "Convert To Fahrenheit";
-            this.btnConvert.UseVisualStyleBackColor = true;
-            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            this.btnFahrenheitConversion.Location = new System.Drawing.Point(48, 240);
+            this.btnFahrenheitConversion.Name = "btnFahrenheitConversion";
+            this.btnFahrenheitConversion.Size = new System.Drawing.Size(121, 23);
+            this.btnFahrenheitConversion.TabIndex = 2;
+            this.btnFahrenheitConversion.Text = "Convert To Fahrenheit";
+            this.btnFahrenheitConversion.UseVisualStyleBackColor = true;
+            this.btnFahrenheitConversion.Click += new System.EventHandler(this.btnConvert_Click);
             // 
             // lblCelsius8Title
             // 
             this.lblCelsius8Title.AutoSize = true;
             this.lblCelsius8Title.Location = new System.Drawing.Point(117, 92);
             this.lblCelsius8Title.Name = "lblCelsius8Title";
-            this.lblCelsius8Title.Size = new System.Drawing.Size(70, 13);
+            this.lblCelsius8Title.Size = new System.Drawing.Size(118, 13);
             this.lblCelsius8Title.TabIndex = 3;
-            this.lblCelsius8Title.Text = "Temp at 8:00";
+            this.lblCelsius8Title.Text = "Temp at 8:00 in Celcius";
             // 
             // lblCelsius12Title
             // 
             this.lblCelsius12Title.AutoSize = true;
             this.lblCelsius12Title.Location = new System.Drawing.Point(117, 128);
             this.lblCelsius12Title.Name = "lblCelsius12Title";
-            this.lblCelsius12Title.Size = new System.Drawing.Size(76, 13);
+            this.lblCelsius12Title.Size = new System.Drawing.Size(124, 13);
             this.lblCelsius12Title.TabIndex = 4;
-            this.lblCelsius12Title.Text = "Temp at 12:00";
+            this.lblCelsius12Title.Text = "Temp at 12:00 in Celcius";
             // 
             // lblCelsius5Title
             // 
             this.lblCelsius5Title.AutoSize = true;
             this.lblCelsius5Title.Location = new System.Drawing.Point(117, 168);
             this.lblCelsius5Title.Name = "lblCelsius5Title";
-            this.lblCelsius5Title.Size = new System.Drawing.Size(70, 13);
+            this.lblCelsius5Title.Size = new System.Drawing.Size(118, 13);
             this.lblCelsius5Title.TabIndex = 5;
-            this.lblCelsius5Title.Text = "Temp at 5:00";
+            this.lblCelsius5Title.Text = "Temp at 5:00 in Celcius";
             // 
             // txtTemp12
             // 
@@ -114,60 +131,229 @@
             // lblFahrenheit8
             // 
             this.lblFahrenheit8.AutoSize = true;
-            this.lblFahrenheit8.Location = new System.Drawing.Point(117, 272);
+            this.lblFahrenheit8.Location = new System.Drawing.Point(45, 272);
             this.lblFahrenheit8.Name = "lblFahrenheit8";
-            this.lblFahrenheit8.Size = new System.Drawing.Size(70, 13);
+            this.lblFahrenheit8.Size = new System.Drawing.Size(134, 13);
             this.lblFahrenheit8.TabIndex = 8;
-            this.lblFahrenheit8.Text = "Temp at 8:00";
+            this.lblFahrenheit8.Text = "Temp at 8:00 in Fahrenheit";
+            this.lblFahrenheit8.Visible = false;
             // 
             // lblFahrenheit12
             // 
             this.lblFahrenheit12.AutoSize = true;
-            this.lblFahrenheit12.Location = new System.Drawing.Point(117, 312);
+            this.lblFahrenheit12.Location = new System.Drawing.Point(45, 303);
             this.lblFahrenheit12.Name = "lblFahrenheit12";
-            this.lblFahrenheit12.Size = new System.Drawing.Size(76, 13);
+            this.lblFahrenheit12.Size = new System.Drawing.Size(140, 13);
             this.lblFahrenheit12.TabIndex = 9;
-            this.lblFahrenheit12.Text = "Temp at 12:00";
+            this.lblFahrenheit12.Text = "Temp at 12:00 in Fahrenheit";
+            this.lblFahrenheit12.Visible = false;
             // 
             // lblFahrenheit5
             // 
             this.lblFahrenheit5.AutoSize = true;
-            this.lblFahrenheit5.Location = new System.Drawing.Point(117, 347);
+            this.lblFahrenheit5.Location = new System.Drawing.Point(45, 338);
             this.lblFahrenheit5.Name = "lblFahrenheit5";
-            this.lblFahrenheit5.Size = new System.Drawing.Size(70, 13);
+            this.lblFahrenheit5.Size = new System.Drawing.Size(134, 13);
             this.lblFahrenheit5.TabIndex = 10;
-            this.lblFahrenheit5.Text = "Temp at 5:00";
+            this.lblFahrenheit5.Text = "Temp at 5:00 in Fahrenheit";
+            this.lblFahrenheit5.Visible = false;
             // 
             // lblResult8
             // 
             this.lblResult8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblResult8.Location = new System.Drawing.Point(281, 272);
+            this.lblResult8.Location = new System.Drawing.Point(216, 271);
             this.lblResult8.Name = "lblResult8";
-            this.lblResult8.Size = new System.Drawing.Size(100, 23);
+            this.lblResult8.Size = new System.Drawing.Size(103, 23);
             this.lblResult8.TabIndex = 11;
-            this.lblResult8.Click += new System.EventHandler(this.label1_Click);
+            this.lblResult8.Visible = false;
             // 
             // lblResult12
             // 
             this.lblResult12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblResult12.Location = new System.Drawing.Point(281, 303);
+            this.lblResult12.Location = new System.Drawing.Point(216, 302);
             this.lblResult12.Name = "lblResult12";
-            this.lblResult12.Size = new System.Drawing.Size(100, 22);
+            this.lblResult12.Size = new System.Drawing.Size(103, 22);
             this.lblResult12.TabIndex = 12;
+            this.lblResult12.Visible = false;
             // 
             // lblResult5
             // 
             this.lblResult5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblResult5.Location = new System.Drawing.Point(281, 338);
+            this.lblResult5.Location = new System.Drawing.Point(216, 337);
             this.lblResult5.Name = "lblResult5";
-            this.lblResult5.Size = new System.Drawing.Size(100, 22);
+            this.lblResult5.Size = new System.Drawing.Size(103, 22);
             this.lblResult5.TabIndex = 13;
+            this.lblResult5.Visible = false;
+            // 
+            // lblAverageFahrenheit
+            // 
+            this.lblAverageFahrenheit.AutoSize = true;
+            this.lblAverageFahrenheit.Location = new System.Drawing.Point(45, 372);
+            this.lblAverageFahrenheit.Name = "lblAverageFahrenheit";
+            this.lblAverageFahrenheit.Size = new System.Drawing.Size(141, 13);
+            this.lblAverageFahrenheit.TabIndex = 14;
+            this.lblAverageFahrenheit.Text = "Average Temp in Fahrenheit";
+            this.lblAverageFahrenheit.Visible = false;
+            // 
+            // lblResultAverageFah
+            // 
+            this.lblResultAverageFah.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultAverageFah.Location = new System.Drawing.Point(216, 371);
+            this.lblResultAverageFah.Name = "lblResultAverageFah";
+            this.lblResultAverageFah.Size = new System.Drawing.Size(103, 22);
+            this.lblResultAverageFah.TabIndex = 15;
+            this.lblResultAverageFah.Visible = false;
+            // 
+            // lblTitleCel8
+            // 
+            this.lblTitleCel8.AutoSize = true;
+            this.lblTitleCel8.Location = new System.Drawing.Point(465, 282);
+            this.lblTitleCel8.Name = "lblTitleCel8";
+            this.lblTitleCel8.Size = new System.Drawing.Size(53, 13);
+            this.lblTitleCel8.TabIndex = 16;
+            this.lblTitleCel8.Text = "In Celcius";
+            this.lblTitleCel8.Visible = false;
+            // 
+            // lblTitleCel12
+            // 
+            this.lblTitleCel12.AutoSize = true;
+            this.lblTitleCel12.Location = new System.Drawing.Point(466, 312);
+            this.lblTitleCel12.Name = "lblTitleCel12";
+            this.lblTitleCel12.Size = new System.Drawing.Size(52, 13);
+            this.lblTitleCel12.TabIndex = 17;
+            this.lblTitleCel12.Text = "In Celsius";
+            this.lblTitleCel12.Visible = false;
+            // 
+            // lblTitleCel5
+            // 
+            this.lblTitleCel5.AutoSize = true;
+            this.lblTitleCel5.Location = new System.Drawing.Point(466, 347);
+            this.lblTitleCel5.Name = "lblTitleCel5";
+            this.lblTitleCel5.Size = new System.Drawing.Size(52, 13);
+            this.lblTitleCel5.TabIndex = 18;
+            this.lblTitleCel5.Text = "In Celsius";
+            this.lblTitleCel5.Visible = false;
+            // 
+            // lblAverageCel
+            // 
+            this.lblAverageCel.AutoSize = true;
+            this.lblAverageCel.Location = new System.Drawing.Point(465, 381);
+            this.lblAverageCel.Name = "lblAverageCel";
+            this.lblAverageCel.Size = new System.Drawing.Size(52, 13);
+            this.lblAverageCel.TabIndex = 19;
+            this.lblAverageCel.Text = "In Celsius";
+            this.lblAverageCel.Visible = false;
+            // 
+            // lblResultCel8
+            // 
+            this.lblResultCel8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultCel8.Location = new System.Drawing.Point(543, 272);
+            this.lblResultCel8.Name = "lblResultCel8";
+            this.lblResultCel8.Size = new System.Drawing.Size(110, 23);
+            this.lblResultCel8.TabIndex = 20;
+            this.lblResultCel8.Visible = false;
+            // 
+            // lblResultCel12
+            // 
+            this.lblResultCel12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultCel12.Location = new System.Drawing.Point(543, 302);
+            this.lblResultCel12.Name = "lblResultCel12";
+            this.lblResultCel12.Size = new System.Drawing.Size(110, 22);
+            this.lblResultCel12.TabIndex = 21;
+            this.lblResultCel12.Visible = false;
+            // 
+            // lblResultCel5
+            // 
+            this.lblResultCel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultCel5.Location = new System.Drawing.Point(543, 337);
+            this.lblResultCel5.Name = "lblResultCel5";
+            this.lblResultCel5.Size = new System.Drawing.Size(110, 22);
+            this.lblResultCel5.TabIndex = 22;
+            this.lblResultCel5.Visible = false;
+            // 
+            // lblResultAverageCel
+            // 
+            this.lblResultAverageCel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultAverageCel.Location = new System.Drawing.Point(543, 371);
+            this.lblResultAverageCel.Name = "lblResultAverageCel";
+            this.lblResultAverageCel.Size = new System.Drawing.Size(110, 22);
+            this.lblResultAverageCel.TabIndex = 23;
+            this.lblResultAverageCel.Visible = false;
+            // 
+            // btnCelsiusConversion
+            // 
+            this.btnCelsiusConversion.Location = new System.Drawing.Point(451, 240);
+            this.btnCelsiusConversion.Name = "btnCelsiusConversion";
+            this.btnCelsiusConversion.Size = new System.Drawing.Size(107, 23);
+            this.btnCelsiusConversion.TabIndex = 24;
+            this.btnCelsiusConversion.Text = "Convert To Celsius";
+            this.btnCelsiusConversion.UseVisualStyleBackColor = true;
+            this.btnCelsiusConversion.Click += new System.EventHandler(this.btnCelsiusConversion_Click);
+            // 
+            // lblMessageF1
+            // 
+            this.lblMessageF1.Location = new System.Drawing.Point(340, 271);
+            this.lblMessageF1.Name = "lblMessageF1";
+            this.lblMessageF1.Size = new System.Drawing.Size(100, 23);
+            this.lblMessageF1.TabIndex = 25;
+            // 
+            // lblMessageF2
+            // 
+            this.lblMessageF2.Location = new System.Drawing.Point(340, 303);
+            this.lblMessageF2.Name = "lblMessageF2";
+            this.lblMessageF2.Size = new System.Drawing.Size(100, 21);
+            this.lblMessageF2.TabIndex = 26;
+            // 
+            // lblMessageF3
+            // 
+            this.lblMessageF3.Location = new System.Drawing.Point(340, 337);
+            this.lblMessageF3.Name = "lblMessageF3";
+            this.lblMessageF3.Size = new System.Drawing.Size(100, 23);
+            this.lblMessageF3.TabIndex = 27;
+            // 
+            // lblMessageC1
+            // 
+            this.lblMessageC1.Location = new System.Drawing.Point(670, 281);
+            this.lblMessageC1.Name = "lblMessageC1";
+            this.lblMessageC1.Size = new System.Drawing.Size(35, 13);
+            this.lblMessageC1.TabIndex = 28;
+            // 
+            // lblMessageC2
+            // 
+            this.lblMessageC2.Location = new System.Drawing.Point(670, 303);
+            this.lblMessageC2.Name = "lblMessageC2";
+            this.lblMessageC2.Size = new System.Drawing.Size(35, 13);
+            this.lblMessageC2.TabIndex = 29;
+            // 
+            // lblMessageC3
+            // 
+            this.lblMessageC3.Location = new System.Drawing.Point(670, 338);
+            this.lblMessageC3.Name = "lblMessageC3";
+            this.lblMessageC3.Size = new System.Drawing.Size(35, 13);
+            this.lblMessageC3.TabIndex = 30;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMessageC3);
+            this.Controls.Add(this.lblMessageC2);
+            this.Controls.Add(this.lblMessageC1);
+            this.Controls.Add(this.lblMessageF3);
+            this.Controls.Add(this.lblMessageF2);
+            this.Controls.Add(this.lblMessageF1);
+            this.Controls.Add(this.btnCelsiusConversion);
+            this.Controls.Add(this.lblResultAverageCel);
+            this.Controls.Add(this.lblResultCel5);
+            this.Controls.Add(this.lblResultCel12);
+            this.Controls.Add(this.lblResultCel8);
+            this.Controls.Add(this.lblAverageCel);
+            this.Controls.Add(this.lblTitleCel5);
+            this.Controls.Add(this.lblTitleCel12);
+            this.Controls.Add(this.lblTitleCel8);
+            this.Controls.Add(this.lblResultAverageFah);
+            this.Controls.Add(this.lblAverageFahrenheit);
             this.Controls.Add(this.lblResult5);
             this.Controls.Add(this.lblResult12);
             this.Controls.Add(this.lblResult8);
@@ -179,7 +365,7 @@
             this.Controls.Add(this.lblCelsius5Title);
             this.Controls.Add(this.lblCelsius12Title);
             this.Controls.Add(this.lblCelsius8Title);
-            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnFahrenheitConversion);
             this.Controls.Add(this.txtTemp8);
             this.Controls.Add(this.lblTitle);
             this.Name = "Form1";
@@ -193,7 +379,7 @@
 
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.TextBox txtTemp8;
-        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnFahrenheitConversion;
         private System.Windows.Forms.Label lblCelsius8Title;
         private System.Windows.Forms.Label lblCelsius12Title;
         private System.Windows.Forms.Label lblCelsius5Title;
@@ -205,6 +391,23 @@
         private System.Windows.Forms.Label lblResult8;
         private System.Windows.Forms.Label lblResult12;
         private System.Windows.Forms.Label lblResult5;
+        private System.Windows.Forms.Label lblAverageFahrenheit;
+        private System.Windows.Forms.Label lblResultAverageFah;
+        private System.Windows.Forms.Label lblTitleCel8;
+        private System.Windows.Forms.Label lblTitleCel12;
+        private System.Windows.Forms.Label lblTitleCel5;
+        private System.Windows.Forms.Label lblAverageCel;
+        private System.Windows.Forms.Label lblResultCel8;
+        private System.Windows.Forms.Label lblResultCel12;
+        private System.Windows.Forms.Label lblResultCel5;
+        private System.Windows.Forms.Label lblResultAverageCel;
+        private System.Windows.Forms.Button btnCelsiusConversion;
+        private System.Windows.Forms.Label lblMessageF1;
+        private System.Windows.Forms.Label lblMessageF2;
+        private System.Windows.Forms.Label lblMessageF3;
+        private System.Windows.Forms.Label lblMessageC1;
+        private System.Windows.Forms.Label lblMessageC2;
+        private System.Windows.Forms.Label lblMessageC3;
     }
 }
 
--- Conversion/Conversion/Form1.cs
+++ Conversion/Conversion/Form1.cs
@@ -17,22 +17,82 @@ namespace Conversion
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-        
         private void btnConvert_Click(object sender, EventArgs e)
         {
-            int celcius8 = Convert.ToInt32(txtTemp8);
-            int celcius12 = Convert.ToInt32(txtTemp12);
-            int celcius5 = Convert.ToInt32(txtTemp5);
+            
+            double celcius8 = Convert.ToDouble(txtTemp8.Text);
+            double celcius12 = Convert.ToDouble(txtTemp12.Text);
+            double celcius5 = Convert.ToDouble(txtTemp5.Text);
             double fahrenheit8 = (celcius8 * 1.8) + 32;
             double fahrenheit12 = (celcius12 * 1.8) + 32;
             double fahrenheit5 = (celcius5 * 1.8) + 32;
+            double average = (fahrenheit8 + fahrenheit12 + fahrenheit5) / 3;
+
+            lblFahrenheit8.Visible = true;
+            lblFahrenheit12.Visible = true;
+            lblFahrenheit5.Visible = true;
+            lblAverageFahrenheit.Visible = true;
+            lblResult8.Visible = true;
+            lblResult12.Visible = true;
+            lblResult5.Visible = true;
+            lblResultAverageFah.Visible = true;
+           
+
+            lblResult8.Text = "" + fahrenheit8;
+            lblResult12.Text = "" + fahrenheit12;
+            lblResult5.Text = "" + fahrenheit5;
+            lblResultAverageFah.Text = "" + average;
+
+            if(fahrenheit8 >= 100)
+            {
+                lblMessageF1.Text = "It is hot outside";
+            }
+            else if(fahrenheit8 <= 32)
+            {
+                lblMessageF1.Text = "It is freezing";
+            }
+
+            if (fahrenheit12 >= 100)
+            {
+                lblMessageF2.Text = "It is hot outside";
+            }
+            else if (fahrenheit12 <= 32)
+            {
+                lblMessageF2.Text = "It is freezing";
+            }
+
+            if (fahrenheit5 >= 100)
+            {
+                lblMessageF3.Text = "It is hot outside";
+            }
+            else if (fahrenheit5 <= 32)
+            {
+                lblMessageF3.Text = "It is freezing";
+            }
+
+        }
+
+        private void btnCelsiusConversion_Click(object sender, EventArgs e)
+        {
+            double celcius8 = Convert.ToDouble(txtTemp8.Text);
+            double celcius12 = Convert.ToDouble(txtTemp12.Text);
+            double celcius5 = Convert.ToDouble(txtTemp5.Text);
+            double celciusAverage = (celcius8 + celcius12 + celcius5) / 3;
+            lblResultCel8.Text = "" + celcius8;
+            lblResultCel12.Text = "" + celcius12;
+            lblResultCel5.Text = "" + celcius5;
+            lblResultAverageCel.Text = "" + celciusAverage;
+
+            lblTitleCel8.Visible = true;
+            lblTitleCel12.Visible = true;
+            lblTitleCel5.Visible = true;
+            lblAverageCel.Visible = true;
+            lblResultCel8.Visible = true;
+            lblResultCel12.Visible = true;
+            lblResultCel5.Visible = true;
+            lblResultAverageCel.Visible = true;
 
 
         }
-        
     }
 }
