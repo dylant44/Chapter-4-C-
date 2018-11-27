fe9bfc6dad9db0ed9294d754aa3e6eee6bf7acdc
dylant44 <dt303271@students.davenport.k12.ia.us>
2018-11-27 13:07:49 -0600

    Guess Game

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber;
+            string guess;
+            int userGuess;
+
+            Random ranNumberGenerator = new Random();
+            randomNumber = ranNumberGenerator.Next(1, 11);
+
+            Console.WriteLine("Guess a number between 1 and 10");
+            guess = Console.ReadLine();
+            userGuess = Convert.ToInt32(guess);
+
+            if(userGuess == randomNumber)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else if(userGuess > randomNumber)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else if(userGuess < randomNumber)
+            {
+                Console.WriteLine("Too Low!");
+            }
+        }
+    }
+}
---
+++ Hurrican/Hurrican/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hurrican
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string WindSpeed;
+            int windSpeed;
+
+            Console.WriteLine("Please enter the wind speed");
+            WindSpeed = Console.ReadLine();
+            windSpeed = Convert.ToInt32(WindSpeed);
+
+            if(windSpeed >= 157)
+            {
+                Console.WriteLine("Category 5 Hurricane");
+            }
+            else if(windSpeed >= 130)
+            {
+                Console.WriteLine("Category 4 Hurricane");
+            }
+            else if (windSpeed >= 111)
+            {
+                Console.WriteLine("Category 3 Hurricane");
+            }
+            else if (windSpeed >= 96)
+            {
+                Console.WriteLine("Category 2 Hurricane");
+            }
+            else if (windSpeed >= 74)
+            {
+                Console.WriteLine("Category 1 Hurricane");
+            }
+            else if (windSpeed < 74)
+            {
+                Console.WriteLine("Storm Is Not A Hurricane");
+            }
+
+        }
+    }
+}
