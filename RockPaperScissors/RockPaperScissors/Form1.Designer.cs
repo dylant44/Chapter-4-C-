0dd8cc0fa35ab77590a4e891c842bd530d066119
dylant44 <dt303271@students.davenport.k12.ia.us>
2018-11-29 12:46:27 -0600

    rock paper scissors accumulator

--- RockPaperScissors/RockPaperScissors/Form1.Designer.cs
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -30,9 +30,11 @@
         {
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblOutput = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
             this.btnRock = new System.Windows.Forms.Button();
+            this.lblWins = new System.Windows.Forms.Label();
+            this.lblWinTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -53,25 +55,25 @@
             this.lblOutput.Size = new System.Drawing.Size(263, 59);
             this.lblOutput.TabIndex = 4;
             // 
-            // button2
+            // btnScissors
             // 
-            this.button2.BackgroundImage = global::RockPaperScissors.Properties.Resources.Scissors;
-            this.button2.Location = new System.Drawing.Point(499, 147);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(314, 145);
-            this.button2.TabIndex = 3;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnScissors.BackgroundImage = global::RockPaperScissors.Properties.Resources.Scissors;
+            this.btnScissors.Location = new System.Drawing.Point(499, 147);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(314, 145);
+            this.btnScissors.TabIndex = 3;
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
+            // btnPaper
             // 
-            this.button1.BackgroundImage = global::RockPaperScissors.Properties.Resources.Paper;
-            this.button1.Location = new System.Drawing.Point(223, 138);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(270, 162);
-            this.button1.TabIndex = 2;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnPaper.BackgroundImage = global::RockPaperScissors.Properties.Resources.Paper;
+            this.btnPaper.Location = new System.Drawing.Point(223, 138);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(270, 162);
+            this.btnPaper.TabIndex = 2;
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnRock
             // 
@@ -83,14 +85,34 @@
             this.btnRock.UseVisualStyleBackColor = true;
             this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
             // 
+            // lblWins
+            // 
+            this.lblWins.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWins.Location = new System.Drawing.Point(633, 378);
+            this.lblWins.Name = "lblWins";
+            this.lblWins.Size = new System.Drawing.Size(85, 24);
+            this.lblWins.TabIndex = 5;
+            // 
+            // lblWinTitle
+            // 
+            this.lblWinTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinTitle.Location = new System.Drawing.Point(649, 343);
+            this.lblWinTitle.Name = "lblWinTitle";
+            this.lblWinTitle.Size = new System.Drawing.Size(53, 28);
+            this.lblWinTitle.TabIndex = 6;
+            this.lblWinTitle.Text = "Wins";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(821, 450);
+            this.Controls.Add(this.lblWinTitle);
+            this.Controls.Add(this.lblWins);
             this.Controls.Add(this.lblOutput);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
             this.Controls.Add(this.btnRock);
             this.Controls.Add(this.lblTitle);
             this.Name = "Form1";
@@ -103,9 +125,11 @@
 
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Button btnRock;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
         private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblWins;
+        private System.Windows.Forms.Label lblWinTitle;
     }
 }
 
--- RockPaperScissors/RockPaperScissors/Form1.cs
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -12,6 +12,7 @@ namespace RockPaperScissors
 {
     public partial class Form1 : Form
     {
+        private static int win = 0;
         public Form1()
         {
             InitializeComponent();
@@ -19,82 +20,79 @@ namespace RockPaperScissors
 
         private void btnRock_Click(object sender, EventArgs e)
         {
-            
-            string result = null;
-            int randomNumber;
-            Random ranNumberGenerator = new Random();
-            randomNumber = ranNumberGenerator.Next(1, 4);
-
-            if (randomNumber == 1)
-            {
-                result = "Tie!";
-            }
-            else if (randomNumber == 2)
-            {
-                result = "You Lose!";
-            }
-            else if (randomNumber == 3)
-            {
-                result = "You Win!";
-            }
-            DisplayCalculation(result);
+            int rock = 1;
+            DisplayCalculation(rock);
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string result = null;
-            int randomNumber;
-            Random ranNumberGenerator = new Random();
-            randomNumber = ranNumberGenerator.Next(1, 4);
-
-            if (randomNumber == 1)
-            {
-                result = "You Win!";
-            }
-            else if (randomNumber == 2)
-            {
-                result = "Tie!";
-            }
-            else if (randomNumber == 3)
-            {
-                result = "You Lose!";
-            }
-            DisplayCalculation(result);
+            int scissors = 3;
+            DisplayCalculation(scissors);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string result = null;
-            int randomNumber;
-            Random ranNumberGenerator = new Random();
-            randomNumber = ranNumberGenerator.Next(1, 4);
-
-            if (randomNumber == 1)
-            {
-                result = "You Lose!";
-            }
-            else if (randomNumber == 2)
-            {
-                result = "You Win!";
-            }
-            else if (randomNumber == 3)
-            {
-                result = "Tie!";
-            }
-            DisplayCalculation(result);
+            int paper = 2;
+            DisplayCalculation(paper);
         }
-        private void DisplayCalculation(string result)
+        private void DisplayCalculation(int decision)
         {
+            string result = null;
+           
             int randomNumber;
             Random ranNumberGenerator = new Random();
             randomNumber = ranNumberGenerator.Next(1, 4);
-
-            if (btnRock_Click = true && randomNumber == 1)
+            if (decision == 1)
             {
-
+                if (randomNumber == 1)
+                {
+                    result = "Tie!";
+                }
+                else if (randomNumber == 2)
+                {
+                    result = "You Lose!";
+                }
+                else if (randomNumber == 3)
+                {
+                    result = "You Win!";
+                    win = 1 + win;
+                }
+            }
+            else if (decision == 2)
+            {
+                if (randomNumber == 1)
+                {
+                    result = "You Lose!";
+                }
+                else if (randomNumber == 2)
+                {
+                    result = "You Win!";
+                    win = 1 + win;
+                }
+                else if (randomNumber == 3)
+                {
+                    result = "Tie!";
+                }
+            }
+            else if (decision == 3)
+            {
+                if (randomNumber == 1)
+                {
+                    result = "You Win!";
+                    win = 1 + win;
+                }
+                else if (randomNumber == 2)
+                {
+                    result = "Tie!";
+                }
+                else if (randomNumber == 3)
+                {
+                    result = "You Lose!";
+                }
             }
             lblOutput.Text = result;
+            lblWins.Text = "" + win;
         }
          
     }
